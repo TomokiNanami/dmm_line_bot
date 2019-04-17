c79bdc468b1389465a8b6d70f6c606def3cc6932
masak <oicwam.orz1228@gmail.com>
2019-04-18 02:06:43 +0900

    【DMM_LineBotをTypescript化プロジェクト～～】ボディパーサー入れてみた

--- src/Server.ts
+++ src/Server.ts
@@ -1,9 +1,12 @@
 import * as express from 'express';
 import router from './router/Router';
+import * as bodyParser from 'body-parser';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use('/', router);
 
 app.listen(PORT, () => {
